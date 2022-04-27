0c47fd46b6934b46aaaf3291c5edf8dde05cb55c
Deivid <deividhugoof@gmail.com>
2022-04-27 15:14:14 -0300

    Git

--- controle/models.py
+++ controle/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Cidade(models.Model):
+    nome = models.CharField(max_length=255)
+
+
+class Clube(models.Model):
+    nome = models.CharField(max_length=255)
+    dono = models.CharField(max_length=255)
+    endereco = models.CharField(max_length=255)
+    dimensao = models.CharField(max_length=255)
+    preco_hora = models.FloatField(max_length=255)
+    cidade = models.ForeignKey(Cidade, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
--- controle/views.py
+++ controle/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from .models import Cidade, Clube
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'web/index.html')
+    cidade = Cidade.objects.all()
+    return render(request, 'web/index.html', {'cidade': cidade, 'coisa': 'Deivid'})
